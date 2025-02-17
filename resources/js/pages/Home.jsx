885615a0fd8db64d1f6313f8195e82cf7a5b7403
mhdhadi12 <hadiparker12@gmail.com>
2025-02-18 00:05:26 +0700

    Listing Resources

--- resources/js/pages/Home.jsx
+++ resources/js/pages/Home.jsx
@@ -1,7 +1,24 @@
-export default function Home() {
+import { Link } from "@inertiajs/react";
+
+export default function Home({ posts }) {
+    console.log(posts);
     return (
         <>
             <h1 className="title">Home hadi</h1>
+
+            <div>
+                {posts.map((post) => (
+                    <div key={post.id} className="p-4 border-b">
+                        <div>
+                            <span className="text-blue-600"> Posted on: </span>
+                            <span>
+                                {new Date(post.created_at).toLocaleString()}
+                            </span>
+                        </div>
+                        <p className="font-medium"> {post.body}</p>
+                    </div>
+                ))}
+            </div>
         </>
     );
 }
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,20 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+Route::get('/', [PostController::class, 'index']);
+
+Route::resource('post', PostController::class)->except('index');
+
 // Route::get('/', function () {
+//     sleep(2);
 //     return inertia::render('Home');
 // });
 
-route::inertia('/', 'Home');
+// route::inertia('/', 'Home');
 
-Route::get('/about', function () {
-    return inertia('about/about');
-});
+// Route::get('/about', function () {
+//     return inertia('about/about');
+// });
